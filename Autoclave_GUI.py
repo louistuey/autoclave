f6934b8d3ec5b25c64ddca71521e2b933fd93842
louistuey <51057386+louistuey@users.noreply.github.com>
2021-03-08 14:39:47 -0800

    Add files via upload
    GUI Updates with Running window functionality

--- Autoclave_GUI.py
+++ Autoclave_GUI.py
@@ -49,26 +49,31 @@ def P3_up():
     Phase3.set(p_3)
     
 def update_clock():
+    global current_time 
     current_time = datetime.datetime.now()
     now = time.strftime("%a %b %d %Y %H:%M:%S")
     timenow.configure(text=now)
-    root.after(200, update_clock)
     p_1 = int(Phase1.get()) 
     p_2 = int(Phase2.get())
     p_3 = int(Phase3.get())
+    root.after(200, update_clock)
     cycle_time = p_1 + p_2 + p_3
     cycle_delta = datetime.timedelta(hours = cycle_time)
     time_finish = current_time + cycle_delta
     print_time_finish = time_finish.strftime("%a %b %d %Y %H:%M")
     finishlabel.configure(text=print_time_finish)
 
+
+
 # creating Tk window
 root = Tk()
+
+
 
 # setting geometry of tk window
 # root.geometry("300x250")
 root.configure(bg = '#333333')
-root.geometry("400x300")
+#root.geometry("400x300")
 #root.attributes('-fullscreen', True)
 
 # Using title() to display a message in
@@ -89,34 +94,41 @@ Phase3.set("4")
 # Use of Entry class to take input from the user
 Phase1Entry= Entry(root, width=3, font=("Arial",14,""),
 				textvariable=Phase1, justify = 'center')
-Phase1Entry.place(x=35,y=50)
+Phase1Entry.grid(column = 2, row = 1)
 
 Phase2Entry= Entry(root, width=3, font=("Arial",14,""),
 				textvariable=Phase2, justify = 'center')
-Phase2Entry.place(x=35,y=92)
+Phase2Entry.grid(column = 2, row = 2)
 
 Phase3Entry= Entry(root, width=3, font=("Arial",14,""),
 				textvariable=Phase3, justify = 'center')
-Phase3Entry.place(x=35,y=134)
+Phase3Entry.grid(column = 2, row = 3)
 
-timenow=Label(font =("Arial Bold",21,""), bg = '#333333', fg = '#63d1db')
-timenow.place(x=10, y=5)
+timenow=Label(font =("Arial Bold",21,""), bg = '#333333', fg = 'White')
+timenow.grid(column = 0, row = 0, columnspan = 4)
 
-phase1label=Label(text = "Pressure", font =("Arial Bold",14), bg = '#333333', fg = '#63d1db')
-phase1label.place(x=100, y=50)
+phase1label=Label(text = "Pressure", font =("Arial Bold",18), bg = '#333333', fg = '#63d1db')
+phase1label.grid(column = 0, row = 1)
 
-phase2label=Label(text = "Pressure + Heat", font =("Arial Bold",14), bg = '#333333', fg = '#63d1db')
-phase2label.place(x=100, y=92)
+phase2label=Label(text = "Pressure + Heat", font =("Arial Bold",18), bg = '#333333', fg = '#63d1db')
+phase2label.grid(column = 0, row = 2)
 
-phase3label=Label(text = "Pressure", font =("Arial Bold",14), bg = '#333333', fg = '#63d1db')
-phase3label.place(x=100, y=134)
+phase3label=Label(text = "Pressure", font =("Arial Bold",18), bg = '#333333', fg = '#63d1db')
+phase3label.grid(column = 0, row = 3)
 
 
 finishtitle=Label(text = "Finish Time", font =("Arial Bold",21), bg = '#333333', fg = '#cf3021')
-finishtitle.place(x=10, y=225)
+finishtitle.grid(column = 0, row = 5, columnspan = 4)
 finishlabel=Label(font =("Arial Bold",21,""), bg = '#333333', fg = '#cf3021')
-finishlabel.place(x=10, y=255)
+finishlabel.grid(column = 0, row = 6, columnspan = 4)
+
 update_clock()
+
+
+
+def exit_win():
+    global Autoclave_Running_Window
+    Autoclave_Running_Window.destroy()
 
 def reset():
     # setting the default phase times
@@ -135,7 +147,16 @@ def submit():
             cycle_delta = datetime.timedelta(hours = cycle_time)
             time_finish = current_time + cycle_delta
             print_time_finish = time_finish.strftime("%a %b %d %Y %H:%M")
-            finished = 0
+            global finished 
+            finished = -1
+            
+            global Autoclave_Running_Window
+            Autoclave_Running_Window = Toplevel(root)
+            Autoclave_Running_Window.title("Autoclave in Use")
+            Label(Autoclave_Running_Window, text = "Autoclave Running").pack()
+            exit_button = Button(Autoclave_Running_Window, text = 'Exit', command = exit_win)
+            exit_button.pack()
+            
             
 	except:
 		print("Please input the right value")
@@ -143,41 +164,37 @@ def submit():
 		
 		finished =1
         
-P1_down_btn = Button(root, text = '<', bd = '3',
+P1_down_btn = Button(root, text = '<', bd = '3',font =("Arial Bold",10),
                        command= P1_down)
-P1_down_btn.place(x = 10,y = 50)
+P1_down_btn.grid(column = 1, row = 1)
 
-P1_up_btn = Button(root, text = '>', bd = '3',
+P1_up_btn = Button(root, text = '>', bd = '3',font =("Arial Bold",10),
                        command= P1_up)
-P1_up_btn.place(x = 77,y = 50)
+P1_up_btn.grid(column = 3, row = 1)
 
-P2_down_btn = Button(root, text = '<', bd = '3',
+P2_down_btn = Button(root, text = '<', bd = '3',font =("Arial Bold",10),
                        command= P2_down)
-P2_down_btn.place(x = 10,y = 92)
+P2_down_btn.grid(column = 1, row = 2)
 
-P2_up_btn = Button(root, text = '>', bd = '3',
+P2_up_btn = Button(root, text = '>', bd = '3',font =("Arial Bold",10),
                        command= P2_up)
-P2_up_btn.place(x = 77,y = 92)
+P2_up_btn.grid(column = 3, row = 2)
 
-P3_down_btn = Button(root, text = '<', bd = '3',
-                       command= P3_down)
-P3_down_btn.place(x = 10,y = 134)
+P3_down_btn = Button(root, text = '<', bd = '3',font =("Arial Bold",10),
+                       state = "disabled", command= P3_down)
+P3_down_btn.grid(column = 1, row = 3)
 
-P3_up_btn = Button(root, text = '>', bd = '3',
-                       command= P3_up)
-P3_up_btn.place(x = 77,y = 134)
+P3_up_btn = Button(root, text = '>', bd = '3',font =("Arial Bold",10),
+                       state = "disabled", command= P3_up)
+P3_up_btn.grid(column = 3, row = 3)
 
-# P2_down_btn = Button(root, text = '<', 
-#                        command = hours_down)
-# hoursdown_btn.place = (x = 25 , y = 30)
-
-start_btn = Button(root, text='Start', bd='5',
+start_btn = Button(root, text='Start', bd='5', bg = 'green', padx = 80, pady = 10,
 			command= submit)
-start_btn.place(x = 100,y = 180)
+start_btn.grid(column = 0, row = 4)
 
-reset_btn = Button(root, text='Reset Times', bd='5',
+reset_btn = Button(root, text='Reset Times', bd='5', bg = 'black' , fg = 'white', pady = 10,
 			command = reset)
-reset_btn.place(x = 160,y = 180)    
+reset_btn.grid(column = 1, row = 4, columnspan = 3)
 
 
 # infinite loop which is required to
